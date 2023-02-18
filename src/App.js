8dc0cf24937f13250b0ecc8c42d080cb92c1882c
Xuski-tip <billkiller.com@gmail.com>
2023-02-18 20:42:33 +0500

    perfect card

--- src/App.js
+++ src/App.js
@@ -9,14 +9,16 @@ import CardMedia from '@mui/material/CardMedia';
 import Search from './components/Search';
 import { useState } from 'react';
 import { Grid } from '@mui/material';
+import { textAlign } from '@mui/system';
 function App() {
   const {search, setSearch} = useState('')  
   const {loading, error, data} = useQuery(ALL_COUNTRIES)
-
+  console.log();
   // Проверка
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
   //
+  console.log(data.countries);
   const handleChange = (e) => {
   }
 
@@ -28,27 +30,48 @@ function App() {
         onChange={handleChange}
       ></Search>
       <Grid container spacing={2}>
-        <Grid item xs='12' md='4'>
-          <Card sx={{ minWidth: 275 }}>
-              <CardContent>
-                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                  Word of the Day
-                </Typography>
-                <Typography variant="h5" component="div">
-                  NAME
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  adjective
-                </Typography>
-                <Typography variant="body2">
-                  well meaning and kindly.
-                  <br />
-                  {'"a benevolent smile"'}
-                </Typography>
-              </CardContent>
-          </Card>
-        </Grid>
-        
+        {data.countries.map(({name, code, currency, continent, languages, phone, capital }) => (
+                <Grid key={code} item xs='12' md='4'>
+                  <Card sx={{ minWidth: 275, minHeight: 250 } }>
+                      <CardContent>
+                        <Typography sx={{textAlign: 'right'}} variant="h6" component="div">
+                            {code}
+                        </Typography>
+                        <Typography variant="h6" component="div">
+                            {name} / {capital}
+                        </Typography>
+
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                          {continent.name}
+                        </Typography>
+                        <Typography sx={{ mb: 0.5, display: 'flex'}}>
+                          Language:{languages.map(responce => {
+                            return (
+                              <Typography  color="text.secondary">
+                                {responce.name + ','} 
+                              </Typography>
+                            )
+                          })}
+                        </Typography>
+                        <Typography sx={{ mb: 1.5, display: 'flex'}}>
+                          Native Language:{languages.map(responce => {
+                            return (
+                              <Typography  color="text.secondary">
+                                {responce.native + ','} 
+                              </Typography>
+                            )
+                          })}
+                        </Typography>
+                        <Typography variant="body2">
+                          Emergency phone number: {phone}
+                        </Typography>
+                        <Typography sx={{textAlign: 'left', mt:2}} variant="h6" component="div">
+                          All currency of country: {currency}
+                        </Typography>
+                      </CardContent>
+                  </Card>
+                </Grid>
+          ))}
       </Grid>
     </div>
   )
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,6 @@ import { TextField } from "@mui/material"
 
 const Search = (props) => {
     const {onChange, value} = props
-    return <TextField label='search' type='search' value={value} onChange={onChange} sx={{mb: 5}} ></TextField>
+    return <TextField label='search by code' type='search' value={value} onChange={onChange} sx={{mb: 5, mt: 5}} ></TextField>
 }
 export default Search
